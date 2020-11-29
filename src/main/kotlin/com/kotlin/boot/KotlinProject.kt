309ff798e295b28402cbc45d9c036d3c13790fdb
dongyoun_shin <dyshin1027@gmail.com>
2020-11-29 23:19:34 +0900

    init2

--- src/main/kotlin/com/kotlin/boot/KotlinProject.kt
+++ src/main/kotlin/com/kotlin/boot/KotlinProject.kt
@@ -1,9 +1,11 @@
 package com.kotlin.boot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [RedisAutoConfiguration::class, RabbitAutoConfiguration::class])
 class KotlinProject
 
 fun main(args: Array<String>) {
