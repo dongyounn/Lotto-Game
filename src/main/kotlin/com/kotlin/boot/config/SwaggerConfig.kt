a24e8ae2e63d4bb156f628ca104148d0132ddeaf
dongyoun_shin <dyshin1027@gmail.com>
2020-11-29 23:22:57 +0900

    머지 테스트

--- src/main/kotlin/com/kotlin/boot/config/SwaggerConfig.kt
+++ src/main/kotlin/com/kotlin/boot/config/SwaggerConfig.kt
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
+
 class SwaggerConfig {
     @Bean
     fun swaggerApi(): Docket = Docket(DocumentationType.SWAGGER_2)
