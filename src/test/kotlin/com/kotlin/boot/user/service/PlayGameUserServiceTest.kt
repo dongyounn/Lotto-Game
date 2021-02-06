a689102c46b2d0ecb1aa976a6c86263c3a6e35c6
dongyoun_shin <dyshin1027@gmail.com>
2021-02-07 01:44:18 +0900

    [test] 유저 생성 테스트 코드, 조회 QueryDsl 테스트 코드

--- src/test/kotlin/com/kotlin/boot/user/service/PlayGameUserServiceTest.kt
+++ src/test/kotlin/com/kotlin/boot/user/service/PlayGameUserServiceTest.kt
@@ -1,9 +1,13 @@
 package com.kotlin.boot.user.service
 
+import com.kotlin.boot.global.dto.NONE
 import com.kotlin.boot.global.sequence.CustomSequenceRepository
+import com.kotlin.boot.global.utils.CustomPageRequest
+import com.kotlin.boot.user.controller.dto.GetUserInfo
 import com.kotlin.boot.user.controller.dto.RegeditUserInfo
 import com.kotlin.boot.user.domain.PlayGameUser
 import com.kotlin.boot.user.infra.repository.PlayGameUserRepository
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -25,24 +29,33 @@ internal class PlayGameUserServiceTest(
             PlayGameUser.of(
                 "1234",
                 RegeditUserInfo(
-                    "990101",
-                    "테스트",
-                    "01011111111",
-                    "똥개"
+                    dob = "990101",
+                    name = "테스트",
+                    phoneNumber = "01011111111",
+                    nickName = "똥개개똥개"
                 )
             )
         )
     }
 
-/*    @BeforeEach
+    @AfterEach
     fun deleteDefaultUserInfo() {
         playGameUserRepository.deleteById("1234")
-    }*/
+    }
 
     @Test
     fun getUserInfo() {
         val result = playGameUserService.getUserInfo("1234")
         Assertions.assertEquals(result.userId, "1234")
+    }
+
+    @Test
+    fun getUserInfoByQueryDslTest() {
+        val result = playGameUserService.getUserInfos(
+            GetUserInfo(null, null, null, "개개"),
+            CustomPageRequest()
+        )
+        Assertions.assertEquals(result?.content?.get(0)?.userId ?: NONE, "1234")
     }
 
     @Test
@@ -59,4 +72,5 @@ internal class PlayGameUserServiceTest(
             )
         )
     }
+
 }
