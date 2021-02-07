a72c02a8b94327403f9f1c4ecf6f5e1ff11b9226
dongyoun_shin <dyshin1027@gmail.com>
2021-02-07 21:48:19 +0900

    [dev] actuator health 추가관련하여 추가 개발 필요

---
+++ src/main/kotlin/com/kotlin/boot/global/config/ApplicationHealthIndicator.kt
@@ -0,0 +1,39 @@
+package com.kotlin.boot.global.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class ApplicationHealthIndicator : HealthIndicator {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+
+    @Value("\${info.version}")
+    private val appVersion: String? = null
+
+    @Value("\${info.git.hash}")
+    private val gitHash: String? = null
+
+    @Value("\${info.git.buildDate}")
+    private val builtDate: String? = null
+
+    @PostConstruct
+    fun init() {
+        log.info("version: {}", appVersion)
+        log.info("hash: {}", gitHash)
+        log.info("date: {}", builtDate)
+    }
+
+    override fun health(): Health {
+        return Health.up()
+            .withDetail("version", appVersion)
+            .withDetail("hash", gitHash)
+            .withDetail("build-date", builtDate)
+            .build()
+    }
+}
