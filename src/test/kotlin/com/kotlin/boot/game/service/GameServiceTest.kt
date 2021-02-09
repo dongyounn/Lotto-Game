39465022ed485b75a68de572dad98946dd157ac2
dongyoun_shin <dyshin1027@gmail.com>
2021-02-09 22:33:46 +0900

    [test] 참여 유저 정보 exception 테스트, 게임 참여 성공 테스트 코드 작성

---
+++ src/test/kotlin/com/kotlin/boot/game/service/GameServiceTest.kt
@@ -0,0 +1,55 @@
+package com.kotlin.boot.game.service
+
+import com.kotlin.boot.game.controller.dto.JoinGameDto
+import com.kotlin.boot.user.controller.dto.RegeditUserInfo
+import com.kotlin.boot.user.domain.PlayGameUser
+import com.kotlin.boot.user.infra.repository.PlayGameUserRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameServiceTest(
+    @Autowired private val gameService: GameService,
+    @Autowired private val playGameUserRepository: PlayGameUserRepository
+) {
+    @BeforeEach
+    fun saveDefaultUserInfo() {
+        playGameUserRepository.save(
+            PlayGameUser.of(
+                "99999999",
+                RegeditUserInfo(
+                    dob = "990101",
+                    name = "테스트",
+                    phoneNumber = "01099992222",
+                    nickName = "똥개개똥개"
+                )
+            )
+        )
+    }
+
+    @AfterEach
+    fun deleteDefaultUserInfo() {
+        playGameUserRepository.deleteById("99999999")
+    }
+
+    @Test
+    fun getPlayGameNumberException() {
+        val req = JoinGameDto(listOf(7, 1, 2, 4), "0109991122")
+        assertThrows(RuntimeException::class.java) {
+            gameService.playGame(req)
+        }
+    }
+
+    @Test
+    fun getPlayGameNumber() {
+        val req = JoinGameDto(listOf(7, 1, 2, 4, 12, 44), "01099992222")
+        gameService.playGame(req)
+        val result = playGameUserRepository.findByPhoneNumber("01099992222")
+        assertEquals(result?.nickName, "똥개개똥개")
+    }
+}
