3523c91e2e963d08e14e12fe78673de40a80dace
dongyoun_shin <dyshin1027@gmail.com>
2021-02-18 23:57:39 +0900

    [test] 게임 참여 테스트 --> 대량으로 참여하고 당첨 테스트 해보자

--- src/test/kotlin/com/kotlin/boot/game/controller/GameControllerTest.kt
+++ src/test/kotlin/com/kotlin/boot/game/controller/GameControllerTest.kt
@@ -1,6 +1,8 @@
 package com.kotlin.boot.game.controller
 
+import com.kotlin.boot.game.controller.dto.JoinGameDto
 import com.kotlin.boot.game.repository.infra.GameResultRepository
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.*
 import javax.transaction.Transactional
 
 @RunWith(SpringRunner::class)
@@ -26,6 +29,17 @@ class GameControllerTest(
     fun createRound() {
         mvc.perform(
             MockMvcRequestBuilders.post("/lotto/create/game/round")
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+    }
+
+    @Test
+//    @RepeatedTest(value = 1000000)
+    fun playGame() {
+        mvc.perform(
+            MockMvcRequestBuilders.post("/lotto/participate")
+                .param("phoneNumber", "01012344121")
                 .accept(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isOk)
