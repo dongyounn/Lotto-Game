62ec562003d47a0ee217eba315554b97bf533949
dongyoun_shin <dyshin1027@gmail.com>
2021-02-20 00:40:24 +0900

    [dev] 게임 추첨 배치 쟉업 페이징 처리 추가

--- src/main/kotlin/com/kotlin/boot/batch/service/BatchService.kt
+++ src/main/kotlin/com/kotlin/boot/batch/service/BatchService.kt
@@ -1,9 +1,13 @@
 package com.kotlin.boot.batch.service
 
 import com.kotlin.boot.game.domain.GameResultEntity
+import com.kotlin.boot.game.repository.infra.GameDataCustomRepository
 import com.kotlin.boot.game.repository.infra.GameRepository
 import com.kotlin.boot.game.repository.infra.GameResultRepository
+import com.kotlin.boot.global.utils.CustomPageRequest
 import com.kotlin.boot.global.utils.NumberUtils
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -12,8 +16,11 @@ import java.util.*
 class BatchService(
     private val gameResultRepository: GameResultRepository,
     private val gameRepository: GameRepository,
+    private val gameDataCustomRepository: GameDataCustomRepository,
     private val numberUtils: NumberUtils
 ) {
+    private val batchSize = 100
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     @Transactional(noRollbackFor = [Exception::class])
     fun playLotto() {
@@ -27,31 +34,43 @@ class BatchService(
         val regularNumber = normalNumber.toString().removeSuffix(",")
 
         val currentRoundInfo = gameResultRepository.findByStatus()
+        val round = currentRoundInfo.id!!
+        val gameCount = gameRepository.countByPlayRound(round)
+        log.info("## 전체 게임 횟수 : $gameCount")
+        log.info("## 배치 사이즈 : $batchSize")
+        val pagingSize = gameCount.div(batchSize).plus(1)
+        var currentPage = 0
 
-        val currentRoundPlayerInfos = gameRepository.findByPlayRound(currentRoundInfo.id!!)
-        currentRoundPlayerInfos.let { gameInfos ->
-            gameInfos.forEach {
-                var matchingCount = 0
-                val matchingNumbers = ArrayList<String>()
-                it?.gameNumber?.split(",")?.forEach { number ->
-                    if (randomNumber.contains(number.toLong())) {
-                        matchingNumbers.add(number)
-                        matchingCount++
+        while (pagingSize > currentPage) {
+            gameDataCustomRepository.getGameDataPagingData(
+                currentRoundInfo.id!!,
+                PageRequest.of(currentPage, batchSize)
+            ).let { gameInfos ->
+                gameInfos.forEach {
+                    var matchingCount = 0
+                    val matchingNumbers = ArrayList<String>()
+                    it?.gameNumber?.split(",")?.forEach { number ->
+                        if (randomNumber.contains(number.toLong())) {
+                            matchingNumbers.add(number)
+                            matchingCount++
+                        }
                     }
+                    val sb = StringBuilder()
+                    matchingNumbers.sorted().forEach { resultNumber -> sb.append("$resultNumber,") }
+                    it?.setDrawResult(
+                        when (matchingCount) {
+                            4 -> 1
+                            3 -> 2
+                            2 -> 3
+                            else -> 0
+                        },
+                        matchingNumbers.toString().removeSuffix(",")
+                    )
                 }
-                val sb = StringBuilder()
-                matchingNumbers.forEach { resultNumber -> sb.append(resultNumber) }
-                it?.setDrawResult(
-                    when (matchingCount) {
-                        4 -> 1
-                        3 -> 2
-                        2 -> 3
-                        else -> 0
-                    },
-                    matchingNumbers.toString()
-                )
             }
+            currentPage++
         }
+
         currentRoundInfo.ofEnd(
             regularNumber
         )
--- src/main/kotlin/com/kotlin/boot/game/repository/infra/GameRepository.kt
+++ src/main/kotlin/com/kotlin/boot/game/repository/infra/GameRepository.kt
@@ -1,9 +1,39 @@
 package com.kotlin.boot.game.repository.infra
 
 import com.kotlin.boot.game.domain.GameEntity
+import com.kotlin.boot.game.domain.QGameEntity.gameEntity
+import com.querydsl.core.BooleanBuilder
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
+import org.springframework.stereotype.Repository
 
 interface GameRepository : JpaRepository<GameEntity, Long> {
     fun findByUserIdAndPlayRound(userId: String, playRound: Long): List<GameEntity>
     fun findByPlayRound(playRound: Long): List<GameEntity?>
+    fun countByPlayRound(playRound: Long): Long
+}
+
+
+@Repository
+class GameDataCustomRepository(
+    private val queryFactory: JPAQueryFactory
+) : QuerydslRepositorySupport(GameEntity::class.java) {
+
+    fun getGameDataPagingData(
+        round: Long,
+        pageRequest: PageRequest
+    ): PageImpl<GameEntity?> {
+        val builder = BooleanBuilder()
+        builder.and(gameEntity.playRound.eq(round))
+
+        val query = queryFactory
+            .selectFrom(gameEntity)
+            .where(builder.value)
+
+        val result = querydsl?.applyPagination(pageRequest, query)?.fetch() ?: ArrayList()
+        return PageImpl(result, pageRequest, query.fetchCount())
+    }
 }
