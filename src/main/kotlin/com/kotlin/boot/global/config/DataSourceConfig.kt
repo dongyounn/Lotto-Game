8b35d67357d26c50b7b0e52f0727d9f9de5d2a0f
dongyoun_shin <dyshin1027@gmail.com>
2021-07-05 22:55:53 +0900

    [dev] h2 기동을 위해서 기존 oracle datasource, hikari 수정

--- src/main/kotlin/com/kotlin/boot/global/config/DataSourceConfig.kt
+++ src/main/kotlin/com/kotlin/boot/global/config/DataSourceConfig.kt
@@ -1,38 +1,40 @@
-package com.kotlin.boot.global.config
+//package com.kotlin.boot.global.config
+//
+//import com.zaxxer.hikari.HikariConfig
+//import com.zaxxer.hikari.HikariDataSource
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.context.annotation.Primary
+//import org.springframework.orm.jpa.JpaTransactionManager
+//import org.springframework.transaction.PlatformTransactionManager
+//import org.springframework.transaction.annotation.EnableTransactionManagement
+//import javax.persistence.EntityManagerFactory
+//
+//@Configuration
+//@EnableTransactionManagement
+//class DataSourceConfig(
+//) {
+//    @Bean
+//    @Primary
+//    internal fun getDataSource(hikariProperties: HikariProperties): HikariDataSource {
+//        val hikariConfig = HikariConfig()
+//        hikariConfig.username = hikariProperties.username
+//        hikariConfig.password = hikariProperties.password
+//        hikariConfig.jdbcUrl = hikariProperties.jdbcUrl
+//        hikariConfig.driverClassName = hikariProperties.driverClassName
+//        hikariConfig.validationTimeout = hikariProperties.validationTimeout
+//        hikariConfig.maximumPoolSize = hikariProperties.maximumPoolSize
+//        hikariConfig.leakDetectionThreshold = hikariProperties.leakDetectionThreshold
+//        return HikariDataSource(hikariConfig)
+//    }
+//
+//    @Bean("transactionManager")
+//    internal fun transactionManager(entityManagerFactory: EntityManagerFactory): PlatformTransactionManager {
+//        val transactionManager = JpaTransactionManager(entityManagerFactory)
+//        transactionManager.entityManagerFactory = entityManagerFactory
+//        return transactionManager
+//    }
+//}
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.orm.jpa.JpaTransactionManager
-import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.annotation.EnableTransactionManagement
-import javax.persistence.EntityManagerFactory
-
-@Configuration
-@EnableTransactionManagement
-class DataSourceConfig(
-) {
-    @Bean
-    @Primary
-    internal fun getDataSource(hikariProperties: HikariProperties): HikariDataSource {
-        val hikariConfig = HikariConfig()
-        hikariConfig.username = hikariProperties.username
-        hikariConfig.password = hikariProperties.password
-        hikariConfig.jdbcUrl = hikariProperties.jdbcUrl
-        hikariConfig.driverClassName = hikariProperties.driverClassName
-        hikariConfig.validationTimeout = hikariProperties.validationTimeout
-        hikariConfig.maximumPoolSize = hikariProperties.maximumPoolSize
-        hikariConfig.leakDetectionThreshold = hikariProperties.leakDetectionThreshold
-        return HikariDataSource(hikariConfig)
-    }
-
-    @Bean("transactionManager")
-    internal fun transactionManager(entityManagerFactory: EntityManagerFactory): PlatformTransactionManager {
-        val transactionManager = JpaTransactionManager(entityManagerFactory)
-        transactionManager.entityManagerFactory = entityManagerFactory
-        return transactionManager
-    }
-}
+//,classpath:/logback.xml
--- src/main/kotlin/com/kotlin/boot/global/config/HikariProperties.kt
+++ src/main/kotlin/com/kotlin/boot/global/config/HikariProperties.kt
@@ -1,16 +1,16 @@
-package com.kotlin.boot.global.config
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ConfigurationProperties(prefix = "spring.datasource.hikari")
-data class HikariProperties(
-    var username: String = "",
-    var password: String = "",
-    var jdbcUrl: String = "",
-    var driverClassName: String = "",
-    var validationTimeout: Long = 5000,
-    var maximumPoolSize: Int = 50,
-    var leakDetectionThreshold: Long = 0
-)
+//package com.kotlin.boot.global.config
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.datasource.hikari")
+//data class HikariProperties(
+//    var username: String = "",
+//    var password: String = "",
+//    var jdbcUrl: String = "",
+//    var driverClassName: String = "",
+//    var validationTimeout: Long = 5000,
+//    var maximumPoolSize: Int = 50,
+//    var leakDetectionThreshold: Long = 0
+//)
